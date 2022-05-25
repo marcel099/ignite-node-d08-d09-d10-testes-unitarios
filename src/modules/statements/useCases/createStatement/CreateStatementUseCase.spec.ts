d8e9022a08abebe94b45dd84a1852ff31d72910c
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-05-25 16:30:02 -0300

    Desenv. 3 testes rest. do use case createStatement

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
 import { OperationType } from "../../entities/Statement";
 import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
+import { CreateStatementError } from "./CreateStatementError";
 import { CreateStatementUseCase } from "./CreateStatementUseCase";
 import { ICreateStatementDTO } from "./ICreateStatementDTO";
 
@@ -30,7 +31,7 @@ describe("Create Statement", () => {
 
   it("should be able to create a new deposit statement", async () => {
     const createStatementRequestData: ICreateStatementDTO = {
-      user_id: userId,  // "fake-id"
+      user_id: userId,
       description: "fake-description",
       amount: 200,
       type: OperationType.DEPOSIT,
@@ -54,5 +55,66 @@ describe("Create Statement", () => {
 
     expect(statement).toHaveProperty("type");
     expect(statement.type).toBe(createStatementRequestData.type);
+  });
+
+  it("should be able to create a new withdraw statement", async () => {
+    await createStatementUseCase.execute({
+      user_id: userId,
+      description: "fake-deposit-description",
+      amount: 250,
+      type: OperationType.DEPOSIT,
+    });
+
+    const createStatementRequestData: ICreateStatementDTO = {
+      user_id: userId,
+      description: "fake-withdraw-description",
+      amount: 100,
+      type: OperationType.WITHDRAW,
+    };
+
+    const statement = await createStatementUseCase.execute(
+      createStatementRequestData
+    );
+
+    expect(statement).toHaveProperty("id");
+    expect(statement.id).toBeTruthy();
+
+    expect(statement).toHaveProperty("user_id");
+    expect(statement.user_id).toBe(userId);
+
+    expect(statement).toHaveProperty("description");
+    expect(statement.description).toBe(createStatementRequestData.description);
+
+    expect(statement).toHaveProperty("amount");
+    expect(statement.amount).toBe(createStatementRequestData.amount);
+
+    expect(statement).toHaveProperty("type");
+    expect(statement.type).toBe(createStatementRequestData.type);
+  });
+
+  it("should not be able to create a new statement due to user not found", async () => {
+    const createStatementRequestData: ICreateStatementDTO = {
+      user_id: "fake-id",
+      description: "fake-description",
+      amount: 200,
+      type: OperationType.DEPOSIT,
+    };
+
+    await expect(
+      createStatementUseCase.execute(createStatementRequestData)
+    ).rejects.toEqual(new CreateStatementError.UserNotFound());
+  });
+
+  it("should not be able to create a new withdraw statement due to insufficient funds", async () => {
+    const createStatementRequestData: ICreateStatementDTO = {
+      user_id: userId,
+      description: "fake-description",
+      amount: 200,
+      type: OperationType.WITHDRAW,
+    };
+
+    await expect(
+      createStatementUseCase.execute(createStatementRequestData)
+    ).rejects.toEqual(new CreateStatementError.InsufficientFunds());
   });
 });
