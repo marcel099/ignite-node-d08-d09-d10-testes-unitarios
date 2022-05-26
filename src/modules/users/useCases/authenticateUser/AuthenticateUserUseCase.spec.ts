024e332656699dd9e0688467107b73f8b2dcee36
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-05-26 13:45:34 -0300

    Desenv. testes do use case authenticateUser

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,91 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+import { IncorrectEmailOrPasswordError } from "./IncorrectEmailOrPasswordError";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let authenticateUserUseCase: AuthenticateUserUseCase;
+
+let compareMocked: jest.SpyInstance;
+let signMocked: jest.SpyInstance;
+
+const tokenValue = "fake-token";
+
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      inMemoryUsersRepository
+    );
+
+    compareMocked = jest.spyOn(bcrypt, "compare").mockImplementation(
+      () => Promise.resolve(true)
+    );
+
+    signMocked = jest.spyOn(jwt, "sign").mockImplementation(
+      () => tokenValue
+    );
+  });
+
+  it("should be able to authenticate a user and return a token in response", async () => {
+    const userCreationData = {
+      name: 'test-name',
+      email: "test@test.com",
+      password: "fake-password",
+    };
+
+    const userAuthenticationData = {
+      email: userCreationData.email,
+      password: userCreationData.password,
+    };
+    
+    await inMemoryUsersRepository.create(userCreationData);
+
+    const authenticationData = await authenticateUserUseCase.execute(userAuthenticationData);
+
+    expect(authenticationData).toHaveProperty("user");
+
+    expect(authenticationData.user).toHaveProperty("id");
+    expect(authenticationData.user.id).toBeTruthy();
+
+    expect(authenticationData.user).toHaveProperty("name");
+    expect(authenticationData.user.name).toBe(userCreationData.name);
+
+    expect(authenticationData.user).toHaveProperty("email");
+    expect(authenticationData.user.email).toBe(userCreationData.email);
+
+    expect(authenticationData).toHaveProperty("token");
+    expect(authenticationData.token).toBe(tokenValue);
+
+    expect(compareMocked).toHaveBeenCalled();
+    expect(signMocked).toHaveBeenCalled();
+  });
+
+  it("should not be able to authenticate a user that doesn't exist", async () => {
+    const userData = {
+      email: "test@test.com",
+      password: "fake-password",
+    }
+
+    expect(
+      authenticateUserUseCase.execute(userData)
+    ).rejects.toEqual(new IncorrectEmailOrPasswordError());
+  });
+
+  it("should not be able to authenticate a user with an incorrect password given", async () => {
+    compareMocked.mockReset().mockImplementation(
+      () => Promise.resolve(false)
+    );
+
+    const userData = {
+      email: "test@test.com",
+      password: "fake-password",
+    }
+
+    expect(
+      authenticateUserUseCase.execute(userData)
+    ).rejects.toEqual(new IncorrectEmailOrPasswordError());
+  });
+});
